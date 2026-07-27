e9dfd1e61ab9c879ef8f87558adb8c0aa7e845bc
nobleman-end <opssc002@gmail.com>
2026-07-27 19:42:11 +0700

    Update index.ts

--- src/Socket/Helpers/index.ts
+++ src/Socket/Helpers/index.ts
@@ -1,1 +1,42 @@
+// MODIFED BY REN X AI
 
+import type { proto } from "../../../WAProto"
+
+export interface SocketHelpers {
+	reply(
+		jid: string,
+		text: string,
+		quoted?: proto.IWebMessageInfo,
+		options?: Record<string, any>
+	): Promise<any>
+}
+
+export const makeHelpers = (
+	sendMessage: Function
+): SocketHelpers => {
+
+	const reply = async (
+		jid: string,
+		text: string,
+		quoted?: proto.IWebMessageInfo,
+		options = {}
+	) => {
+
+		return sendMessage(
+			jid,
+			{
+				text,
+				...options
+			},
+			{
+				quoted
+			}
+		)
+
+	}
+
+	return {
+		reply
+	}
+
+}
