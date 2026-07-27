5d4ab2733b70003e65dddd5e744fc2b20e9e8a4e
nobleman-end <opssc002@gmail.com>
2026-07-27 21:42:48 +0700

    Update Interactive.ts

--- src/Utils/Interactive.ts
+++ src/Utils/Interactive.ts
@@ -1,23 +1,26 @@
 import { proto } from "../../WAProto"
 import { ButtonMessage } from "../Types/Button"
+import { InteractiveHeader } from "../Types/Interactive"
+
 import { buildBody } from "./Interactive/Body"
 import { buildFooter } from "./Interactive/Footer"
-import { buildHeader } from "./Interactive/Header"
 import { buildNativeFlow } from "./Interactive/NativeFlow"
+
 export const generateButtonMessage = (
-	message: ButtonMessage
+	message: ButtonMessage,
+	header?: InteractiveHeader
 ) => {
 
 	return proto.Message.InteractiveMessage.create({
 
-	header: buildHeader(message),
+		header: header?.header,
 
-	body: buildBody(message.text),
+		body: buildBody(message.text),
 
-	footer: buildFooter(message.footer),
+		footer: buildFooter(message.footer),
 
-	nativeFlowMessage: buildNativeFlow(message.buttons)
+		nativeFlowMessage: buildNativeFlow(message.buttons)
 
-})
+	})
 
 }
